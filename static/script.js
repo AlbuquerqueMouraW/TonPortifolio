f00665727d22d10307a053e63351aebb68ba1d5c
Washington Albuquerque Moura <walbuque@gmail.com>
2021-12-08 00:29:15 -0300

    finalizacao e config deploy

--- static/script.js
+++ static/script.js
@@ -3,9 +3,9 @@ const menuMobile = document.querySelector('.menu-mobile')
 const body = document.querySelector('body')
 
 menuMobile.addEventListener("click", () => {
-  menuMobile.classList.contains("bi-list-ul") 
-  ? menuMobile.classList.replace("bi-list-ul", "bi-x")
-  : menuMobile.classList.replace("bi-x", "bi-list-ul");
+  menuMobile.classList.contains("bi-list") 
+  ? menuMobile.classList.replace("bi-list", "bi-x")
+  : menuMobile.classList.replace("bi-x", "bi-list");
 
   body.classList.toggle("menu-nav-active");
 });
@@ -44,3 +44,4 @@ animeScroll()
 window.addEventListener("scroll", ()=> {
   animeScroll();
 })
+
